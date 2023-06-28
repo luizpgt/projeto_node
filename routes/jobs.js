e704acf2711cd941cc6f99a6024efce70cab9d13
luizpgt <luiz.paulus51@gmail.com>
2023-06-28 14:46:28 -0300

    feat: view da job individual

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,6 +8,16 @@ router.get('/test', (req, res) => {
   res.send('deu certo');
 });
 
+// detalhe da vaga "view/1"
+router.get('/view/:id', (req, res) => Job.findOne ({
+  where: {id: req.params.id}
+}).then (job => {
+  res.render('view', {
+  job
+  });
+}).catch (err => console.log(err)));
+
+// form rota envio
 router.get('/add', (req, res) => {
   res.render('layouts/add');
 })
